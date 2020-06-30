d8e99a9e0647900a6e200a9bba8611ad863815ad
Gabriela Rodrigues <andre.quintero96@gmail.com>
2020-06-29 21:37:27 -0300

    usando css modules no componente de busca

--- src/components/form-perfil.tsx
+++ src/components/form-perfil.tsx
@@ -1,13 +1,14 @@
 import React, { FC } from 'react';
 import FormPerfilModel from '../model/Form/form-perfil.model';
+import styles from '../css/search.module.css';
 
 const Form: FC<FormPerfilModel> = ( { handleSubmit, onChangeInput, placeholder = '', title = '' }) => (
-    <div className=" profile-input-container">
-    <div><h1>{title}</h1></div>
-    <form onSubmit={handleSubmit} className="in-center">
-        <input onChange={onChangeInput} className="profile-input " placeholder={placeholder} type="search"/> 
-        <button type="submit" className="profile-button"></button>
-    </form>   
+    <div className={styles.profileInputContainer}>
+        <div><h1>{title}</h1></div>
+        <form onSubmit={handleSubmit} className={styles.inCenter}>
+            <input onChange={onChangeInput} className={styles.profileInput} placeholder={placeholder} type="search"/> 
+            <button type="submit" className={styles.profileButton}></button>
+        </form>   
     </div> 
 );
 
